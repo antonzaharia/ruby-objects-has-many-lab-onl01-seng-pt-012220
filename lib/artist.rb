0c2b179e047b3bcba319fa416eaba46b81a2e89a
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-10 14:55:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,11 @@
 class Artist 
   
-  @@songs = []
+  @songs = []
   attr_accessor :name
   def initialize(name)
     @name = name
   end
   def songs
-    @@songs << Song.self
+    @songs << Song.self
   end
 end
