bf2cb11fa48c427552a3d516fdda938c2646c06c
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-10 15:25:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,8 +12,8 @@ class Song
     @@all
   end
   def artist_name
-    if self.artist.name.legnth == 0 
-      nil 
+    if self.artist.name.length == 0 
+      return nil
     else
       self.artist.name
     end
