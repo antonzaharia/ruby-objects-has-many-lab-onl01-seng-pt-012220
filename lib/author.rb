d110abbe8cd57c76d1e97bbcbed207b18bcad5f4
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-10 15:49:14 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,9 @@
 class Author 
   
   attr_accessor :name
-  @@posts = []
   
   def initialize(name)
     @name = name
-    @@posts << self
   end
   def posts
     Post.all.select{|post| post.author == self}
